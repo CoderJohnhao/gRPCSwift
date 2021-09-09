730213c4e4141c459d3a89908a4662c423029986
JH <x_johnhao@163.com>
2021-09-09 22:29:35 +0800

    Initial Commit

---
+++ gRPCDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gRPCDemo
+//
+//  Created by Johnhao on 2021/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
